d03dfae4f861e9e158b727f5a53b01c5d4f8c870
simran s <sps022011@gmail.com>
2025-07-29 11:26:17 -0700

    added affirmation page

---
+++ affirmationpage.swift
@@ -0,0 +1,21 @@
+//
+//  affirmationpage.swift
+//  SerenitySpace
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct affirmationpage: View {
+    var body: some View {
+        ZStack {
+            Color(.bgcolor)
+                .ignoresSafeArea()
+        }//zstack
+    }//body
+}//struct
+
+#Preview {
+    affirmationpage()
+}
