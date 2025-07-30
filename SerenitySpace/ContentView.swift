7b2b90c736f278e61b62e5c4d2872db9d48f9987
Piyali Jina <piyallijina@gmail.com>
2025-07-30 11:24:28 -0700

    Testing again

--- SerenitySpace/ContentView.swift
+++ SerenitySpace/ContentView.swift
@@ -4,28 +4,80 @@
 //
 //  Created by Scholar on 7/29/25.
 //
+//Color.red
+//  .ignoreSafeSpace
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Change 1")
-            Text("i made a change yippee")
-                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
-            Text("Change 2")
-            Text("Change 2.5")
-            Text("new change - simran")
-            Text("New change-Piyali")
-            Text("changey changey")
-        }
-        .padding()
-    }
-}
+        NavigationStack {
+                ZStack {
+                    //for adding color
+                    Color(.color3)
+                        .ignoresSafeArea()
+                    ScrollView{
+                        
+                        VStack{
+                            //title
+                            Text("⋆｡°✩ 𝕄𝕖𝕟𝕥𝕒𝕝 ✩°｡⋆")
+                                .font(.largeTitle)
+                                .padding(8)
+                            Text("𝕙𝕖𝕒𝕝𝕥𝕙")
+                                .font(.largeTitle)
+                            
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.color4)
+                                    .frame(width: 359, height: 200)
+                                Text("Text description")
+                                    .multilineTextAlignment(.trailing)
+                                
+                            }//zstack
+                            
+                            Text("𝕐𝕠𝕦 𝕒𝕣𝕖𝕟'𝕥 𝕒𝕝𝕠𝕟𝕖.")
+                                .font(.largeTitle)
+                            
+                            HStack (spacing:30){
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.color1)
+                                    .frame(width: 90, height: 90)
+                                
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.color1)
+                                    .frame(width: 90, height: 90)
+                                
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.color1)
+                                    .frame(width: 90, height: 90)
+                            }//Hstack
+                            
+                            Image("sunset")
+                                .cornerRadius(20)
+                            
+                            
+                            
+                            Text("ℂ𝕠𝕞𝕞𝕠𝕟 𝕚𝕤𝕤𝕦𝕖𝕤")
+                                .font(.largeTitle)
+                                .multilineTextAlignment(.trailing)
+                            
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.color2)
+                                .frame(width: 359, height: 150)
+                            
+                            
+                            
+                            
+                            Spacer()
+                            
+                            
+                        }//Zstack
+                    }//scrollview
+            }//VStack
+        }//navstack
+    }//body
+}//struct
 
 #Preview {
     ContentView()
