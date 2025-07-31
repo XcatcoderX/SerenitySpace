6e0c7ea02bf379884c0f2d574ba6220e9dd73693
simran s <sps022011@gmail.com>
2025-07-31 15:00:53 -0700

    keeping it on the same page - simran

---
+++ SerenitySpace/endpage.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+struct endingpage: View {
+  var body: some View {
+    ZStack{
+      Color(.bgcolor)
+        .ignoresSafeArea()
+      ScrollView {
+          ZStack {
+              VStack {
+                  
+              }//
+              
+          }
+      }
+    }//zstack
+  }//body
+}//struct
+#Preview {
+  endingpage()
+}
+
